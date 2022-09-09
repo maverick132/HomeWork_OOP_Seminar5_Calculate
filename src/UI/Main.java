74a5286c3136a05ba05ba7dffdf2d162d20cc915
Александр Лёвин <maverick132@rambler.ru>
2022-09-10 01:01:15 +0300

    Fix

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -8,19 +8,6 @@ public class Main {
     public static void main(String[] args) {
 
         new App().start();
-//throws UnacceptableValueException
-
-//        SumModelComplex n2 = new SumModelComplex();
-//        SumModelComplex n3 = new SumModelComplex();
-//        SumModelComplex n4 = new SumModelComplex();
-
-//        Presenter p = new Presenter(new ConsoleView(), new CalcModel());
-
-//        p.loadLog(Config.pathLog);
-
-
-//        p.saveLog(Config.pathLog);
-
 
     }
 }
